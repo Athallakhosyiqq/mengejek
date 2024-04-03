9135635932d749415f5a17319009a2e64d345c5f
Athallakhosyiqq <156729019+Athallakhosyiqq@users.noreply.github.com>
2024-04-03 20:28:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+gsap.from('.gambar', {
+    duration: '2',
+    x: '200%'
+})
+
+gsap.from('.hai', {
+    duration: '2',
+    x: '-200%'
+})
+
+gsap.from('.header', {
+    duration: '2',
+    y: '-100%'
+})
